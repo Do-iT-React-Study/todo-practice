de2ac08d570ca83fe3a1f8a1606f49f8e297d3bf
SangJin521 <rhrhghrhrhgh@ajou.ac.kr>
2023-11-02 19:19:11 +0900

    [sangjin] 4주차 출석과제

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,52 @@
+import {MdAdd} from 'react-icons/md'
+import styled from 'styled-components';
+
+export const TodoInsert = () => {
+    return (
+        <InsertBox>
+            <input placeholder="할 일을 입력하시오"></input>
+            <button><MdAdd/></button>
+        </InsertBox>
+    );
+};
+
+
+const InsertBox = styled.div`
+    display: flex;
+    background: #495057;
+    
+    input {
+        background: none;
+        outline: none;
+        border: none;
+
+        padding: 0.5rem;
+        font-size: 1.125rem;
+        line-height: 1.5;
+        color: white;
+
+        &::placeholder {
+            color: #dee2e6;
+        }
+
+        flex: 1;
+    }
+    button {
+        background: #868e96;
+        outline: none;
+        border: none;
+        color: white;
+        width: 50px;
+        font-size: 2rem;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        cursor: pointer;
+
+        &:hover {
+            background: pink;
+        }
+    }
+    `;
